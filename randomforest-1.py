6e75380cea3c479995df65bd84a6378c5289bbb2
miao-zh <mzmooc@163.com>
2024-10-09 22:19:26 +0800

    标准化和归一化数据

--- randomforest-1.py
+++ randomforest-1.py
@@ -7,6 +7,7 @@ from sklearn.metrics import accuracy_score
 import matplotlib
 from openpyxl import load_workbook
 from sklearn.model_selection import cross_val_score
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
 
 #读取数据
 df = pd.read_excel(r'0614-熔融 结晶 全反射 3类.xlsx')
@@ -23,6 +24,16 @@ df = pd.read_excel(r'0614-熔融 结晶 全反射 3类.xlsx')
 X = df.drop(columns=['samples','classes'])
 Y=df['classes']
 
+# 标准化
+scaler = StandardScaler()
+X_scaled = scaler.fit_transform(X)
+# 归一化
+scaler = MinMaxScaler()
+X_normalized = scaler.fit_transform(X)
+print(X_normalized)
+print(X_scaled)
+
+"""
 # 将数据集分为训练集和测试集
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.3, random_state=42)
 
@@ -41,3 +52,4 @@ print(f"模型准确率: {accuracy:.2f}")
 # 进行 5 折交叉验证
 cv_scores = cross_val_score(clf, X, Y, cv=5)
 print(f"交叉验证的平均准确率: {cv_scores.mean():.2f}")
+"""
